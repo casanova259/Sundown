67b62d222369c4ae4694440d5bd3fd612273e848
Manik Sharma <casanova270407@gmail.com>
2025-12-24 14:50:03 +0530

    made that pic effect footer stuck on swipper

--- script..js
+++ script..js
@@ -1,6 +1,42 @@
+// const scroll = new LocomotiveScroll({
+//   el: document.querySelector("#main"),
+//   smooth: true,
+// });
 
+function Page4Animation() {
+  var elemC = document.querySelector("#elem-container");
+  var fixedImg = document.querySelector("#fixed-image");
+  elemC.addEventListener("mouseenter", function () {
+    fixedImg.style.display = "block";
+  });
 
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#main'),
-    smooth: true
-});
+  elemC.addEventListener("mouseleave", function () {
+    fixedImg.style.display = "none";
+  });
+
+  //apan query selector all krke sare elem ko select krege
+  //aur uski img kko loop ki help se lga dege
+  var elems = document.querySelectorAll(".elem");
+
+  //yeh data mere pass aayega node list ki form me which is kindA Like array
+  elems.forEach(function (e) {
+    e.addEventListener("mouseenter", function () {
+      var image = e.getAttribute("data-image");
+      // console.log(image);
+      fixedImg.style.backgroundImage = `url(${image})`;
+    });
+  });
+}
+function swiperAnimation() {
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+}
+swiperAnimation();
+Page4Animation();
