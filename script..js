f170c199dc1e39d09a7225e3f273bf24203a1fa6
Manik Sharma <casanova270407@gmail.com>
2025-12-24 21:40:55 +0530

    completed the website almost res and bb and swi

--- script..js
+++ script..js
@@ -38,5 +38,35 @@ function swiperAnimation() {
     },
   });
 }
+
+function menuAnimate() {
+  var menu = document.querySelector("nav h3");
+  var full = document.querySelector("#full-SCR");
+  var navImg = document.querySelector("nav img");
+  var flag = 0;
+  menu.addEventListener("click", function () {
+    if (flag == 0) {
+      full.style.top = 0;
+      navImg.style.opacity = 0;
+      flag = 1;
+      // console.log(flag);
+    } else {
+      full.style.top = "-100%";
+      navImg.style.opacity = 1;
+      flag = 0;
+      console.log("BSDK");
+    }
+  });
+}
+
+
 swiperAnimation();
 Page4Animation();
+menuAnimate();
+
+var loader=document.querySelector("#loader");
+//set timeout=delay thodi der ka wait
+setTimeout(function()
+{
+  loader.style.top="-100%";
+},4000)
